f1a062e80d711945a48f47a899820630ccb21c55
daniel <danielbonnells1@gmail.com>
2024-04-12 10:23:56 -0400

    debug sfdd

--- ESP32_MTA_Feed/Program.cs
+++ ESP32_MTA_Feed/Program.cs
@@ -1,13 +1,13 @@
-//var builder = WebApplication.CreateBuilder(args);
+var builder = WebApplication.CreateBuilder(args);
 
-var ops = new WebApplicationOptions
-{
-    ContentRootPath = AppContext.BaseDirectory,
-    EnvironmentName = "Production",
-};
+// var ops = new WebApplicationOptions
+// {
+//     ContentRootPath = AppContext.BaseDirectory,
+//     EnvironmentName = "Production",
+// };
 
 
-var builder = WebApplication.CreateBuilder(ops);
+// var builder = WebApplication.CreateBuilder(ops);
 
 
 
